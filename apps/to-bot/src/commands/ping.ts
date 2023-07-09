9d838518f5fa462ab959598a5e4477611190e2ad
Gabriel Dias Mazieri <gabrieldias7200@gmail.com>
2023-07-09 00:15:37 -0300

    wip bot commands

---
+++ apps/to-bot/src/commands/ping.ts
@@ -0,0 +1,10 @@
+import { SlashCommandBuilder } from "discord.js";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with pong!'),
+    async execute(interaction) {
+        await interaction.reply('Pong!');
+    }
+}
--- apps/to-bot/src/index.ts
+++ apps/to-bot/src/index.ts
@@ -1,11 +1,24 @@
-import { Client } from 'discord.js';
+import { 
+  Client,
+  Events,
+  GatewayIntentBits,
+  SlashCommandBuilder
+} from 'discord.js';
 
 if(!process.env.DISCORD_TOKEN) throw new Error('Invalid token value on environment');
 
 const client = new Client({
-  intents: [],
+  intents: [ GatewayIntentBits.Guilds ],
   partials: [],
 });
+
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+// will receive a Task and will create a reminder to the user sending a notification
+
 
 client.login(process.env.DISCORD_TOKEN)
   .then(() => console.log('running'))
