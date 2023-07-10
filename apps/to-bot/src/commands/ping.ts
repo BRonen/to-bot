8b30f83e4271470704e227551e25f2da594df5ea
Gabriel Dias Mazieri <gabrieldias7200@gmail.com>
2023-07-10 18:09:04 -0300

    wip: formatar o pc

--- apps/to-bot/src/commands/ping.ts
+++ apps/to-bot/src/commands/ping.ts
@@ -1,10 +1,14 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, CommandInteraction} from "discord.js";
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with pong!'),
-    async execute(interaction) {
+
+    async execute(interaction: CommandInteraction) {
+        console.log(interaction);
+        
         await interaction.reply('Pong!');
+
     }
 }
