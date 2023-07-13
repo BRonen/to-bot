2c625cf63599ab4616d9e18158016987192e924f
Brenno Rodrigues <brennopereira6@gmail.com>
2023-07-13 17:40:18 -0300

    style(bot): formatting

--- apps/to-bot/src/index.ts
+++ apps/to-bot/src/index.ts
@@ -1,12 +1,14 @@
-import { Client } from 'discord.js';
+import { Client } from "discord.js";
 
-if(!process.env.DISCORD_TOKEN) throw new Error('Invalid token value on environment');
+if (!process.env.DISCORD_TOKEN)
+  throw new Error("Invalid token value on environment");
 
 const client = new Client({
   intents: [],
   partials: [],
 });
 
-client.login(process.env.DISCORD_TOKEN)
-  .then(() => console.log('running'))
+client
+  .login(process.env.DISCORD_TOKEN)
+  .then(() => console.log("running"))
   .catch(console.error);
