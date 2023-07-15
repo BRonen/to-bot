0e5fd749593569c500aeb72d3ebd76c8d0fe4844
Brenno Rodrigues <brennopereira6@gmail.com>
2023-07-15 15:43:04 -0300

    feat: initial to-do module routes

---
+++ apps/api/src/routes/to-do.routes.ts
@@ -0,0 +1,11 @@
+import Router from "@koa/router";
+
+const router = new Router({
+  prefix: "/to-do",
+});
+
+router.get("/", async (ctx) => {
+  ctx.body = "hello world";
+});
+
+export default router;
