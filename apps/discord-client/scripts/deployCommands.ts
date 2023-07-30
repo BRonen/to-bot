9deaf3602ab18e85abe0596e20ef545236c91a48
Brenno Rodrigues <brennopereira6@gmail.com>
2023-07-30 11:21:35 -0300

    refactor(discord-client): using command loaders on commands deploy

--- apps/discord-client/scripts/deployCommands.ts
+++ apps/discord-client/scripts/deployCommands.ts
@@ -1,7 +1,5 @@
 import { REST, Routes } from "discord.js";
-import * as fs from "node:fs";
-import * as path from "node:path";
-import { CommandHandler } from "../src/commands/Command";
+import { loadCommandHandlers } from "../src/commands/loadCommandHandlers";
 
 if (!process.env.DISCORD_TOKEN)
   throw new Error("Invalid DISCORD_TOKEN value on environment");
@@ -12,27 +10,10 @@ if (!process.env.CLIENT_ID)
 if (!process.env.GUILD_ID)
   throw new Error("Invalid GUILD_ID value on environment");
 
-const commandsFolderPath = path.join(__dirname, "../src/commands");
-
-type commandHadlerPromise = Promise<{ default: CommandHandler }>;
-const commandHandlerByFilename = (
-  commandFilename: string
-): commandHadlerPromise =>
-  import(path.join(commandsFolderPath, commandFilename));
-
-const commandsHandlersPromises = fs
-  .readdirSync(commandsFolderPath)
-  .filter((file) => file.endsWith(".command.ts"))
-  .map(commandHandlerByFilename);
-
-Promise.all(commandsHandlersPromises).then(async (commandFilePaths) => {
+loadCommandHandlers().then(async (commandFilePaths) => {
   const rest = new REST().setToken(process.env.DISCORD_TOKEN!);
 
   try {
-    console.log(
-      `Started deploying ${commandFilePaths.length} application (/) commands.`
-    );
-
     await rest.put(
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID!,
@@ -40,7 +21,7 @@ Promise.all(commandsHandlersPromises).then(async (commandFilePaths) => {
       ),
       {
         body: commandFilePaths.map((commandFilePath) =>
-          commandFilePath.default.data.toJSON()
+          commandFilePath.data.toJSON()
         ),
       }
     );
