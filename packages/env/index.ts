72df496c3ec058afd88a9e0bbcb665ce40db8a5d
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-14 01:40:40 -0300

    fix(env): scheduler environment load

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,24 +1,35 @@
 import "dotenv/config";
 
-// implement a checking to type safe environment loading
+// TODO: implement a checking to type safe environment loading
 
 export type ApiEnvironment = {
   PORT: number;
-  DATABASE_CONFIG: "development" | "production";
   DATABASE_URI: string;
 };
 
 export const loadApiEnvironment = async (): Promise<ApiEnvironment> => {
   const environment = {
     PORT: process.env.PORT as unknown as number,
-    DATABASE_CONFIG: process.env.DATABASE_CONFIG as unknown as
-      | "development"
-      | "production",
     DATABASE_URI: process.env.DATABASE_URI as unknown as string,
   };
 
   return environment;
 };
+
+export type SchedulerEnvironment = {
+  PORT: number;
+  DATABASE_URI: string;
+};
+
+export const loadSchedulerEnvironment =
+  async (): Promise<SchedulerEnvironment> => {
+    const environment = {
+      DATABASE_URI: process.env.DATABASE_URI as unknown as string,
+      PORT: process.env.PORT as unknown as number,
+    };
+
+    return environment;
+  };
 
 export type DatabaseEnvironment = {
   DATABASE_URI: string;
