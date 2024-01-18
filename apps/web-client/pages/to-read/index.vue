bc6ca48ca595fc8330d5962b4c7809ffd76093c7
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-18 20:30:06 -0300

    feat(web): adding to-read page

---
+++ apps/web-client/pages/to-read/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const tab = ref<'to-read' | 'to-read-keyword'>('to-read')
+</script>
+<template>
+  <to-read-data-table v-if="tab === 'to-read'">
+    <template #tooltip>
+      <v-tabs v-model="tab">
+        <v-tab value="to-read">
+          To-Read
+        </v-tab>
+        <v-tab value="to-read-keyword">
+          To-Read-Keyword
+        </v-tab>
+      </v-tabs>
+    </template>
+  </to-read-data-table>
+  <to-read-keyword-data-table v-else-if="tab === 'to-read-keyword'">
+    <template #tooltip>
+      <v-tabs v-model="tab">
+        <v-tab value="to-read">
+          To-Read
+        </v-tab>
+        <v-tab value="to-read-keyword">
+          To-Read-Keyword
+        </v-tab>
+      </v-tabs>
+    </template>
+  </to-read-keyword-data-table>
+</template>
