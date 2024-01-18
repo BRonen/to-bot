c53fb35d6f61ddb55c254887ee25f35dc41d7a64
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-18 20:29:05 -0300

    chore(web): vuetify nuxt plugin setup

---
+++ apps/web-client/plugins/vuetify.ts
@@ -0,0 +1,11 @@
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({ components, directives });
+  app.vueApp.use(vuetify);
+});
