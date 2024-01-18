1ebe8b0db467c551182e44ab1ae2eef8b2679208
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-18 20:16:54 -0300

    feat(api): adding cors

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,19 +2,21 @@ import Koa, { type DefaultState } from "koa";
 
 import bodyParser from "@koa/bodyparser";
 import Router from "@koa/router";
+import cors from "@koa/cors";
 
-import type { ApiEnvironment } from "@to-bot/env";
+import type { loadApiEnvironment } from "@to-bot/env";
 import type { AppContext, Database } from "./types";
 
 export const createApp = (
   routes: Router<DefaultState, AppContext>,
   database: Database,
-  _environment: ApiEnvironment
+  _environment: ReturnType<typeof loadApiEnvironment>
 ) => {
   const app = new Koa<DefaultState, AppContext>();
 
   return app
     .use(bodyParser())
+    .use(cors())
     .use(async (ctx, next) => {
       ctx.database = database;
       await next();
