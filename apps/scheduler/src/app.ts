ad0f30505425c0658a08e39a1383e0a30eae66ba
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-18 20:15:37 -0300

    feat(scheduler): adding cors

--- apps/scheduler/src/app.ts
+++ apps/scheduler/src/app.ts
@@ -1,19 +1,21 @@
 import Koa, { type DefaultState } from "koa";
 
 import bodyParser from "@koa/bodyparser";
+import cors from "@koa/cors";
 import Router from "@koa/router";
 
-import type { ApiEnvironment } from "@to-bot/env";
+import type { loadApiEnvironment } from "@to-bot/env";
 import type { AppContext, Database, Cron } from "./types";
 
 export const createApp = (
   routes: Router<DefaultState, AppContext>,
   database: Database,
   cron: Cron,
-  _environment: ApiEnvironment
+  _environment: ReturnType<typeof loadApiEnvironment>
 ) => {
   const app = new Koa<DefaultState, AppContext>()
     .use(bodyParser())
+    .use(cors())
     .use(async (ctx, next) => {
       ctx.database = database;
       ctx.cron = cron;
@@ -21,5 +23,5 @@ export const createApp = (
     })
     .use(routes.routes())
     .use(routes.allowedMethods());
-  return app
+  return app;
 };
