0aa4ec786481414ee05a3701d57057b8f646a01d
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-18 20:20:10 -0300

    feat(database): adding logger on drizzle

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,11 +1,18 @@
-import { PostgresJsDatabase, PostgresJsTransaction, drizzle } from "drizzle-orm/postgres-js";
-import { ExtractTablesWithRelations } from "drizzle-orm"
+import {
+  PostgresJsDatabase,
+  PostgresJsTransaction,
+  drizzle,
+} from "drizzle-orm/postgres-js";
+import { ExtractTablesWithRelations } from "drizzle-orm";
 import postgres from "postgres";
 
 import * as schema from "./schemas";
 
 export type Database = PostgresJsDatabase<typeof schema>;
-export type Transaction = PostgresJsTransaction<typeof schema, ExtractTablesWithRelations<typeof schema>>;
+export type Transaction = PostgresJsTransaction<
+  typeof schema,
+  ExtractTablesWithRelations<typeof schema>
+>;
 
 export const createClient = (uri: string) => {
   return postgres(uri, { max: 1 });
@@ -14,5 +21,5 @@ export const createClient = (uri: string) => {
 export const createConnection = (uri: string) => {
   const client = createClient(uri);
 
-  return drizzle(client, { schema });
+  return drizzle(client, { logger: true, schema });
 };
