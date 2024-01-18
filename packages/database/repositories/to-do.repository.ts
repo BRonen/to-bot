602d5e07e4b205eb2b42aa02103cf0b949d2a59e
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-18 20:23:44 -0300

    refactor(database): linting

--- packages/database/repositories/to-do.repository.ts
+++ packages/database/repositories/to-do.repository.ts
@@ -3,116 +3,111 @@ import type { Database, Transaction } from "../";
 import { toDoSchema } from "../schemas";
 
 export type ToDoDto = {
-    id: number;
-    name: string;
-    archived: boolean;
-    status: number | null;
-    created_at: number;
-    updated_at: number;
+  id: number;
+  name: string;
+  archived: boolean;
+  status: number | null;
+  created_at: number;
+  updated_at: number;
 };
 
 export type CreateToDoDto = {
-    name: string;
+  name: string;
 };
 
 export type UpdateToDoDto = {
-    name: string;
+  name: string;
 };
 
 export type ToDoRepository = {
-    find: (id: string) => Promise<ToDoDto | undefined>;
-    findAll: () => Promise<ToDoDto[]>;
-    create: (
-        createToDoDto: CreateToDoDto
-    ) => Promise<ToDoDto>;
-    update: (
-        updateToDoDto: UpdateToDoDto,
-        id: string
-    ) => Promise<ToDoDto>;
-    delete: (id: string) => Promise<ToDoDto>;
-    transaction: <T>(callback: (trx: Transaction) => Promise<T>) => Promise<T>;
+  find: (id: string) => Promise<ToDoDto | undefined>;
+  findAll: () => Promise<ToDoDto[]>;
+  create: (createToDoDto: CreateToDoDto) => Promise<ToDoDto>;
+  update: (updateToDoDto: UpdateToDoDto, id: string) => Promise<ToDoDto>;
+  delete: (id: string) => Promise<ToDoDto>;
+  transaction: <T>(callback: (trx: Transaction) => Promise<T>) => Promise<T>;
 };
 
 const createRepository = (db: Database): ToDoRepository => ({
-    find: async (id) => {
-        const [result] = await db
-            .select()
-            .from(toDoSchema)
-            .where(eq(toDoSchema.id, Number(id)));
+  find: async (id) => {
+    const [result] = await db
+      .select()
+      .from(toDoSchema)
+      .where(eq(toDoSchema.id, Number(id)));
 
-        return {
-            id: result.id,
-            name: result.name,
-            archived: result.archived,
-            status: result.status,
-            created_at: result.createdAt.getTime(),
-            updated_at: result.updatedAt.getTime(),
-        };
-    },
+    return {
+      id: result.id,
+      name: result.name,
+      archived: result.archived,
+      status: result.status,
+      created_at: result.createdAt.getTime(),
+      updated_at: result.updatedAt.getTime(),
+    };
+  },
 
-    findAll: async () => {
-        const results = await db.select().from(toDoSchema);
+  findAll: async () => {
+    const results = await db.select().from(toDoSchema);
 
-        return results.map((result) => ({
-            id: result.id,
-            name: result.name,
-            archived: result.archived,
-            status: result.status,
-            created_at: result.createdAt.getTime(),
-            updated_at: result.updatedAt.getTime(),
-        }));
-    },
+    return results.map((result) => ({
+      id: result.id,
+      name: result.name,
+      archived: result.archived,
+      status: result.status,
+      created_at: result.createdAt.getTime(),
+      updated_at: result.updatedAt.getTime(),
+    }));
+  },
 
-    create: async (createToDoDto) => {
-        const [result] = await db
-            .insert(toDoSchema)
-            .values(createToDoDto)
-            .returning();
+  create: async (createToDoDto) => {
+    const [result] = await db
+      .insert(toDoSchema)
+      .values(createToDoDto)
+      .returning();
 
-        return {
-            id: result.id,
-            name: result.name,
-            archived: result.archived,
-            status: result.status,
-            created_at: result.createdAt.getTime(),
-            updated_at: result.updatedAt.getTime(),
-        };
-    },
+    return {
+      id: result.id,
+      name: result.name,
+      archived: result.archived,
+      status: result.status,
+      created_at: result.createdAt.getTime(),
+      updated_at: result.updatedAt.getTime(),
+    };
+  },
 
-    update: async (updateToDoDto, id) => {
-        const [result] = await db
-            .update(toDoSchema)
-            .set(updateToDoDto)
-            .where(eq(toDoSchema.id, Number(id)))
-            .returning();
+  update: async (updateToDoDto, id) => {
+    const [result] = await db
+      .update(toDoSchema)
+      .set(updateToDoDto)
+      .where(eq(toDoSchema.id, Number(id)))
+      .returning();
 
-        return {
-            id: result.id,
-            name: result.name,
-            archived: result.archived,
-            status: result.status,
-            created_at: result.createdAt.getTime(),
-            updated_at: result.updatedAt.getTime(),
-        };
-    },
+    return {
+      id: result.id,
+      name: result.name,
+      archived: result.archived,
+      status: result.status,
+      created_at: result.createdAt.getTime(),
+      updated_at: result.updatedAt.getTime(),
+    };
+  },
 
-    delete: async (id) => {
-        const [result] = await db
-            .delete(toDoSchema)
-            .where(eq(toDoSchema.id, Number(id)))
-            .returning();
+  delete: async (id) => {
+    const [result] = await db
+      .delete(toDoSchema)
+      .where(eq(toDoSchema.id, Number(id)))
+      .returning();
 
-        return {
-            id: result.id,
-            name: result.name,
-            archived: result.archived,
-            status: result.status,
-            created_at: result.createdAt.getTime(),
-            updated_at: result.updatedAt.getTime(),
-        };
-    },
+    return {
+      id: result.id,
+      name: result.name,
+      archived: result.archived,
+      status: result.status,
+      created_at: result.createdAt.getTime(),
+      updated_at: result.updatedAt.getTime(),
+    };
+  },
 
-    transaction: (callback) => db.transaction(callback),
+  transaction: (callback) => db.transaction(callback),
 });
 
 export default createRepository;
