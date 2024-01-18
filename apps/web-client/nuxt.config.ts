b22bc047a64cc10b8cfeda53717449b0e8a62a39
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-18 20:28:06 -0300

    chore(web): adding vue query

--- apps/web-client/nuxt.config.ts
+++ apps/web-client/nuxt.config.ts
@@ -1,4 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    "@hebilicious/vue-query-nuxt",
+    "dayjs-nuxt",
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        config.plugins && config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 });
